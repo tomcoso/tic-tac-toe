fba5adfe6be9f420e5db3230f710865c8fc954c8
tomcoso <tomasdessy0@gmail.com>
2022-07-05 16:54:54 +0100

    implement game initialisation

--- main.js
+++ main.js
@@ -49,45 +49,62 @@ let gameboard = (function() {
 
     // Cache DOM
     let htmlBoard = document.querySelectorAll('.cell');
-    htmlBoard.forEach(each => {each.addEventListener('click', _selectPlayer)})
+    
 
 
-    let _render = function() {
+    let _render = function(player, position) {
+        console.log('render:',player, position);
+
+        for (let i = 0; i < htmlBoard.length ; i++) {
+            if (htmlBoard[i] === position) {
+                let index = htmlBoard[i].attributes[1].value
+                let symbolIndex = player.length - 1;
+                gameboard[index] = player[symbolIndex];
+            }
+        }
+
         for ( let i = 0 ; i < gameboard.length ; i++ ) {
             htmlBoard[i].textContent = gameboard[i];
         }
     }
 
-    _render();
+    let _restart = function(player1) {
 
-    let _restart = function() {
+        console.log('restart', player1)
+
         gameboard.forEach((element, index) => {
             gameboard[index] = null;
         });
+
+        firstMove = player1.id;
+
         _render();
     }
 
-    var _selectPlayer = function() {
+    let _selectPlayer = function() {
+        console.log('selectPlayer:')
         switch (lastPlay) {
             case 'playero' :
-                pubsub.publish('playerMove', 'playerx');
+                _render('playerx', this);
                 lastPlay = 'playerx';
                 break;
             case 'playerx' :
-                pubsub.publish('playerMove', 'playero');
+                _render('playero', this);
                 lastPlay = 'playero';
                 break;
             case '' :
-                pubsub.publish('playerMove', firstMove);
+                _render(firstMove, this);
                 if (firstMove === 'playerx') {
-                    lastPlay = 'playero';
+                    lastPlay = 'playerx';
                 }
                 else {
-                    lastPlay = 'playerx';
+                    lastPlay = 'playero';
                 }
                 break;
         }
     }
+
+    htmlBoard.forEach(each => each.addEventListener('click', _selectPlayer))
 
     let _checkStatus = function() {
         let boardMatch = '';
@@ -113,6 +130,9 @@ let gameboard = (function() {
         }
     }
 
+    pubsub.subscribe('newGame', _restart);
+    pubsub.subscribe('makeMove', _render)
+
     return { seeArray, _restart, _checkStatus };
 })(); 
 
@@ -126,27 +146,67 @@ let gameboard = (function() {
 let Player = function(name, symbol) {
 
     this.name = name;
-    this.id = `player${symbol}`;
+    this.id = `player${symbol.toLowerCase()}`;
     this.score = 0
 
-    let _play = function(index) {
-        pubsub.publish('play', index);
+    let play = function(player, position) {
+        console.log('play', player, position, this.id)
+        if (this.id === player) {
+            
+            pubsub.publish('makeMove', [player, position]);
+        }
     }
 
-    let _changeScore = function() {
+    // let _changeScore = function() {
 
-    }
-    pubsub.subscribe('changeStatus', _changeScore);
+    // }
+    // pubsub.subscribe('changeStatus', _changeScore);
 
-    let _resetCount = function() {
+    // let _resetCount = function() {
 
-    }
+    // }
+
+    // pubsub.subscribe('selectMove', play);
+
+    return { id, play };
 
 }
 
 let display = (function() {
 
     // cache DOM
-    
+    let player1Name = document.querySelector('.player1 p').textContent;
+    let player2Name = document.querySelector('.player2 p').textContent;
 
+    let player1Count = document.querySelector('.player1 .count');
+    let player2Count = document.querySelector('.player2 .count');
+
+    let player1Symbol = document.querySelector('.symbol1').textContent;
+    let player2Symbol = document.querySelector('.symbol2').textContent;
+
+    let startGamePanel = document.querySelector('.start-container');
+    let startGameBtn = document.querySelector('#start-game');
+
+    let _setPlayer = function() {
+
+        let player1 = Player(player1Name, player1Symbol);
+        let player2 = Player(player2Name, player2Symbol);
+
+        console.log('setPlayer', player1, player2);
+
+        return { player1, player2 };
+    }
+
+    let _startGame = function() {
+
+        const { player1, player2 } = _setPlayer();
+
+        pubsub.publish('newGame', player1);
+
+        startGamePanel.classList.add('hidden');
+    }
+        
+    startGameBtn.addEventListener('click', _startGame);
+
+    return {  }
 })();
